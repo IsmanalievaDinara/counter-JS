0557eb8121aee0140530683602ea0f0024f6e45b
Dinara <ismanalievadina@gmail.com>
2022-03-23 10:20:05 +0300

    added script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const span = document.querySelector("#vaule");
+const decreaseBtn = document.querySelector(".decrease");
+const resetBtn = document.querySelector(".reset")
+const increaseBtn = document.querySelector(".increase");
+
+let count = 0;
+
+
+decreaseBtn.addEventListener("click",function() {
+    count--
+    span.innerText = count;
+})
+resetBtn.addEventListener("click",function() {
+    count = 0
+    span.innerText = count;
+})
+increaseBtn.addEventListener("click",function() {
+    count++
+    span.innerText = count;
+})
+
